441cf7c17989870c837d679474cbc05d842e9c36
Pedro Durand <pedrodurand.dev@gmail.com>
2025-03-20 18:20:35 -0300

    feat: audio recorder

--- frontend/src/components/Recorder/index.js
+++ frontend/src/components/Recorder/index.js
@@ -1,31 +1,53 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 
 export function Recorder() {
-    const [frequency, setFrequency] = useState()
+    const [isRecording, setIsRecording] = useState(false);
+    const [audioUrl, setAudioUrl] = useState("");
+    const mediaRecorder = useRef(null);
+    const audioChunks = useRef([]);
 
-    function handleButtonClick(){
-        if (!frequency) return;
+    async function startRecording() {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        mediaRecorder.current = new MediaRecorder(stream);
 
-        const context = new (window.AudioContext || window.webkitAudioContext)();
-        const osc = context.createOscillator()
-        osc.connect(context.destination)
+        mediaRecorder.current.ondataavailable = (event) => {
+            if (event.data.size > 0) {
+                audioChunks.current.push(event.data);
+            }
+        };
 
-        osc.frequency.value = Number(frequency)
+        mediaRecorder.current.onstop = () => {
+            const audioBlob = new Blob(audioChunks.current, { type: "audio/wav" });
+            const url = URL.createObjectURL(audioBlob);
+            setAudioUrl(url);
+            audioChunks.current = [];
+        };
 
-        osc.start()
-        osc.stop(context.currentTime + 1)
+        mediaRecorder.current.start()
+        setIsRecording(true);
+    }
 
-        return () => {
-            osc.disconnect()
-            context.close()
-        }
+    function stopRecording() {
+        if (mediaRecorder.current) {
+            mediaRecorder.current.stop();
+            setIsRecording(false);
+          }
     }
 
     return (
         <div>
-            <h1>Digite a frequência</h1>
-            <input type="number" value={frequency} onChange={e => setFrequency(e.target.value)} />
-            <button onClick={handleButtonClick}>Tocar</button>
+            <h1>Gravação de áudio</h1>
+            {isRecording && <button onClick={stopRecording}>Parar</button>}
+            {!isRecording && <button onClick={startRecording}>Gravar</button>}
+
+            {audioUrl &&
+                <>
+                    <h2>Reproduzir gravação</h2>
+                    <audio controls src={audioUrl} />
+                    <href src={audioUrl} download={"teste.wav"}>Baixar gravação</href>
+                </>
+
+            }
         </div>
     )
 }

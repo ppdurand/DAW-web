8d5dab451ed05b02f5e8fa22b425a548579cdd48
Pedro Paulo Durand <pedropaulodurand@gmail.com>
2025-03-21 09:19:02 -0300

    fesat: add waveSurfer to vizualize audios

--- frontend/src/components/TrackList/index.js
+++ frontend/src/components/TrackList/index.js
@@ -1,16 +1,40 @@
+import { useEffect, useState } from "react";
 import "./style.css";
+import WaveSurfer from "wavesurfer.js"
 
 export function TrackList({ audioUrls }) {
+    const [currentIndex, setCurrentIndex] = useState()
+
+    useEffect(() => {
+        setCurrentIndex(audioUrls.length)
+        audioUrls.forEach(audio => {
+            if (audioUrls[currentIndex] == audio) {
+                const waveSurfer = WaveSurfer.create({
+                    container: `#waveform-${currentIndex}`,
+                    waveColor: 'green',
+                    progressColor: 'darkgreen',
+                    barWidth: 2,
+                    height: 80
+                })
+
+                waveSurfer.load(audio)
+                console.log(waveSurfer)
+
+                return () => waveSurfer.destroy()
+            }
+            return null;
+        })
+    }, [audioUrls])
     return (
         <div className="records-list">
             {audioUrls.length > 0 && (
                 <>
                     <h2>Reproduzir gravação</h2>
                     {audioUrls.map((url, index) => (
-                        <div key={index}>
+                        <>
                             <audio controls src={url} />
-                            <a href={url} download={`gravacao_${index + 1}.wav`}>Baixar gravação</a>
-                        </div>
+                            <div key={index} id={`waveform-${index}`} />
+                        </>
                     ))}
                 </>
             )}

4c7312995c08fee4f6c66ede346611a525bfe8c8
Pedro Paulo Durand <pedropaulodurand@gmail.com>
2025-04-01 10:57:56 -0300

    backend initial commit

---
+++ ThunderSound/ThunderSound/src/main/java/edu/ThunderSound/ThunderSoundApplication.java
@@ -0,0 +1,13 @@
+package edu.ThunderSound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThunderSoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThunderSoundApplication.class, args);
+	}
+
+}

34fdb65ac0432a0c993dcedd0e9558a6fe1bd27a
Pedro Paulo Durand <pedropaulodurand@gmail.com>
2025-04-21 22:28:38 -0300

    FIX: RECOMEÇANDO BACKEND

--- backend/java/src/main/java/edu/ThunderSound/application/dto/ProjectDTO.java
+++
@@ -1,9 +0,0 @@
-package edu.ThunderSound.application.dto;
-
-import edu.ThunderSound.domain.model.Track;
-import java.util.List;
-
-public class ProjectDTO {
-    public record CreateProjectDTO(String name, List<Track> tracks) {
-    }
-}
